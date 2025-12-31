7aa651eba0f450dbcb35636920240795d50b5f75
sai kiran <saikiranmedisetti2003@gmail.com>
2025-12-31 13:10:26 +0530

    commitnew

---
+++ MavenDemo/src/test/java/Graphs/JavaDay1Test.java
@@ -0,0 +1,15 @@
+package Graphs;
+
+public class JavaDay1Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Day1");
+		
+		String a=System.getProperty("a");
+		String b=System.getProperty("b");
+		System.out.println(a+b);
+
+	}
+
+}
